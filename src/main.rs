14d16ef04709861fc4d4c8cb54608bb4e14b0f83
Evan <evan.perreau@gmail.com>
2024-02-29 13:39:03 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+slint::include_modules!();
+
+const TAXPER: f64 = 0.30;
+const OWNERPER: f64 = 0.55;
+const PROFITPER: f64 = 0.05;
+const OPEXPER: f64 = 0.10;
+
+fn main() -> Result<(), slint::PlatformError> {
+    let ui = AppWindow::new()?;
+
+    let ui_handle = ui.as_weak();
+    ui.on_divide_income(move |string| {
+        let ui = ui_handle.unwrap();
+        match string.trim().parse::<f64>() {
+            Ok(num) => {
+                let taxes: f64 = num * TAXPER;
+                let owner: f64 = num * OWNERPER;
+                let profit: f64 = num * PROFITPER;
+                let opex: f64 = num * OPEXPER;
+                let result = format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOpEx: {:.2}", taxes, owner, profit, opex);
+                ui.set_results(result.into());
+            },
+            Err(_) => {
+                ui.set_results("Invalid input".into());
+            }
+        }
+    });
+
+    ui.run()
+}
